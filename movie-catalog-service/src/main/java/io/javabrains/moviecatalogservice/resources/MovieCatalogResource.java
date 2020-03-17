e67cf030fa8888f3ae78be1a717ac6161844c6a4
dddawiddd <dawidn@onet.eu>
2020-03-17 21:38:18 +0100

    02. Added movie catalog API

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,23 @@
+package io.javabrains.moviecatalogservice.resources;
+
+import io.javabrains.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @GetMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        return Collections.singletonList(
+                new CatalogItem("Transformers", "Test", 4));
+    }
+
+
+}
