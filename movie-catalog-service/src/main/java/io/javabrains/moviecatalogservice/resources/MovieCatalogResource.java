369e765e72ef6189f421c436938fa3cea0ae8628
dddawiddd <dawidn@onet.eu>
2020-03-18 22:26:24 +0100

    Using @Bean and @Autowired for RestTemplate instance

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package io.javabrains.moviecatalogservice.resources;
 import io.javabrains.moviecatalogservice.models.CatalogItem;
 import io.javabrains.moviecatalogservice.models.Movie;
 import io.javabrains.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,9 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
@@ -30,9 +34,9 @@ public class MovieCatalogResource {
         return ratings.stream().map(rating -> {
 
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-             return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
         })
                 .collect(Collectors.toList());
-        
+
     }
 }
